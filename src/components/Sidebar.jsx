aca2d4275e1b7fa7f79e4ed693d268afe87931a8
srijan008 <srijankumar008@gmail.com>
2025-02-06 23:49:44 +0530

    add isActive in Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,15 @@
-
 import datinglogo from '../assets/datinglogo.png'
-import {Link, useNavigate} from 'react-router-dom'
+import {Link, useLocation} from 'react-router-dom'
 import { useSetRecoilState } from 'recoil'
 import { isLoggedin } from '../atoms/loginatom'
 
 const Sidebar = () => {
     const setlogin = useSetRecoilState(isLoggedin);
+    const location = useLocation();
+    
+    const isActive = (path) => {
+        return location.pathname === path;
+    };
     
     const hadnleLogout = ()=>{
         setlogin(false);
@@ -30,7 +34,12 @@ const Sidebar = () => {
                 </Link>
                 <ul className="space-y-2 font-medium">
                 <li>
-                        <Link to="/admin/users" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                        <Link 
+                            to="/admin/users" 
+                            className={`flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group ${
+                                isActive('/admin/users') ? 'bg-gray-100 dark:bg-gray-700' : ''
+                            }`}
+                        >
                         <svg className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
                             <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z"/>
                         </svg>
@@ -38,24 +47,39 @@ const Sidebar = () => {
                         </Link>
                     </li>
                     <li>
-                        <Link to="/admin/classifiedform" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                        <Link 
+                            to="/admin/classifiedform" 
+                            className={`flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group ${
+                                isActive('/admin/classifiedform') ? 'bg-gray-100 dark:bg-gray-700' : ''
+                            }`}
+                        >
                         <svg className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 18">
                             <path d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z"/>
                         </svg>
-                        <span className="flex-1 ms-3 whitespace-nowrap">Classified Forms </span>
+                        <span className="flex-1 ms-3 whitespace-nowrap">Classified Forms</span>
                         </Link>
                     </li>
                     <li>
-                        <Link to="/admin/advertisements" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                        <Link 
+                            to="/admin/advertisements" 
+                            className={`flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group ${
+                                isActive('/admin/advertisements') ? 'bg-gray-100 dark:bg-gray-700' : ''
+                            }`}
+                        >
                         <svg className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
                             <path d="M19 3H5c-1.1 0-1.99.9-1.99 2L3 19c0 1.1.9 2 1.99 2H19c1.1 0 1.99-.9 1.99-2V5c0-1.1-.9-2-1.99-2zm-1 16H6V8h12v11zM6 6V5h12v1H6z"/>
                             </svg>
 
-                        <span className="flex-1 ms-3 whitespace-nowrap">All Ads </span>
+                        <span className="flex-1 ms-3 whitespace-nowrap">All Ads</span>
                         </Link>
                     </li>
                     <li>
-                        <Link to="/admin/charts" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                        <Link 
+                            to="/admin/charts" 
+                            className={`flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group ${
+                                isActive('/admin/charts') ? 'bg-gray-100 dark:bg-gray-700' : ''
+                            }`}
+                        >
                         <svg className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
                         <path d="M3 3v18h18V3H3zm16 16H5V5h14v14zm-4-4h-3V9h3v6zm-5 0H7V7h3v8zm-5 0H2v4h3v-4z"/>
                         </svg>
@@ -63,17 +87,13 @@ const Sidebar = () => {
                         <span className="flex-1 ms-3 whitespace-nowrap">Insights</span>
                         </Link>
                     </li>
-                    {/* <li>
-                        <Link to="/admin/admincomments" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                        <svg className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-                        <path d="M3 3v18h18V3H3zm16 16H5V5h14v14zm-4-4h-3V9h3v6zm-5 0H7V7h3v8zm-5 0H2v4h3v-4z"/>
-                        </svg>
-
-                        <span className="flex-1 ms-3 whitespace-nowrap">Admin Comments</span>
-                        </Link>
-                    </li> */}
                     <li>
-                        <Link to="/admin/superadminprofile" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                        <Link 
+                            to="/admin/superadminprofile" 
+                            className={`flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group ${
+                                isActive('/admin/superadminprofile') ? 'bg-gray-100 dark:bg-gray-700' : ''
+                            }`}
+                        >
                         <svg
                             className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white border border-gray-400 rounded-lg"
                             aria-hidden="true"
--- src/pages/Admin/CreateUser.jsx
+++ src/pages/Admin/CreateUser.jsx
@@ -133,13 +133,13 @@ const CreateUserForm = () => {
             disabled
           />
         </div>
-        <button
+        {/* <button
             onClick={() => setLoading(true)}
           type="submit"
           className={`w-full px-4 py-2 text-white bg-blue-500 rounded-md shadow-md hover:bg-blue-600 focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 ` + (loading ? " cursor-not-allowed bg-gray-200" : "")}
         >
             {loading ? "Creating User..." : "Create User"}
-        </button>
+        </button> */}
       </form>
     </div>
   );
